e9439dcf1de54fc62cf53ec755b37792ce17747d
TechlabRS <129305203+TechlabRS@users.noreply.github.com>
2023-05-16 22:58:46 +0530

    Update my_r53.tf

--- my_tf_rs/01-vars/my_r53.tf
+++ my_tf_rs/01-vars/my_r53.tf
@@ -1,9 +1,9 @@
 resource "aws_route53_zone" "main" {
-  name = "example.com"
+  name = "raghas.com"
 }
 
 resource "aws_route53_zone" "dev" {
-  name = "dev.example.com"
+  name = "dev.raghas.com"
 
   tags = {
     Environment = "dev"
@@ -12,7 +12,7 @@ resource "aws_route53_zone" "dev" {
 
 resource "aws_route53_record" "dev-ns" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = "dev.example.com"
+  name    = "dev.raghas.main.com"
   type    = "NS"
   ttl     = "30"
   records = aws_route53_zone.dev.name_servers
