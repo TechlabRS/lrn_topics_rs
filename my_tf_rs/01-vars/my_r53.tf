4c4eef842ca89058c0acd1e8fa296de7db83e67e
TechlabRS <129305203+TechlabRS@users.noreply.github.com>
2023-05-16 23:03:30 +0530

    Update my_r53.tf

--- my_tf_rs/01-vars/my_r53.tf
+++ my_tf_rs/01-vars/my_r53.tf
@@ -10,10 +10,11 @@ resource "aws_route53_zone" "dev" {
   }
 }
 
-resource "aws_route53_record" "dev-ns" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "dev.raghas.main.com"
-  type    = "NS"
-  ttl     = "30"
-  records = aws_route53_zone.dev.name_servers
-}
+# may be its not recorded causes dups
+# resource "aws_route53_record" "dev-ns" {
+#   zone_id = aws_route53_zone.main.zone_id
+#   name    = "dev.raghas.main.com"
+#   type    = "NS"
+#   ttl     = "30"
+#   records = aws_route53_zone.dev.name_servers
+# } 
